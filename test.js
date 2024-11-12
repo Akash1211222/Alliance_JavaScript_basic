d928cda7ba7748ee281d7f6bbfd42f2ecdc962c4
Akash Paul <90515043+Akash1211222@users.noreply.github.com>
2024-11-12 03:57:17 +0000

    sec A

--- test.js
+++ test.js
@@ -1,12 +1,2 @@
-// console.log(accountId)
-// var Name = "Akash"
-// Name = "Adil"
-// console.log(Name)
-// var a = 5;
-// var b = 6;
-// b=10
-// a = 1.2
-// console.log(a)
-// console.log(b)
-const accountId = 12345
-accountId = 9999
+console.log("Hello jii");
+alert("hello")
